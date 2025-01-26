4de49783107430e8137396d9f10160665590ed64
YourUsername <your_email@example.com>
2025-01-27 00:36:15 +0100

    creation workflow

--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,33 @@
-import os
 import pandas as pd
-
-def process_excel(file_path):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"Le fichier '{file_path}' est introuvable.")
+def process_data(file_path, output_path):
+    # Load the dataset
+    df = pd.read_csv(file_path)
     
-    # Lecture du fichier Excel
-    df = pd.read_excel(file_path)
-    print("Contenu du fichier Excel :")
-    print(df)
+   
+    
+    # Step 2: Remove unwanted rows (like a specific index)
+    df.drop(100, inplace=True)
+        
+    # Step 5: Remove duplicate rows
+    df.drop_duplicates(inplace=True)
+    
+   # Nettoyage de base
+    df.dropna(subset=['Country'], inplace=True)  # Supprime les chaînes sans pays
+    df['Subscribers'] = df['Subscribers'].str.replace('M', 'e6').astype(float)  # Convertir "M" en valeurs numériques
+    df['Views'] = df['Views'].str.replace(',', '').astype(float)  # Enlever les virgules et convertir en float
+    df['Uploads'] = df['Uploads'].str.replace(',', '').astype(float)
+    # Step 7: Save the cleaned dataset to a new Excel file
+    df.to_csv(output_path, index=False)
+    
+    # Display dataset summary
+    print("Cleaned Data Overview:")
+    print(df.head())
+    print("\nDataset Description:")
+    print(df.describe())
 
 if __name__ == "__main__":
-    # Chemin relatif vers le fichier Excel
-    file_path = "data/note.xlsx"
+    input_file = "data/YOUTUBE_CHANNELS_DATASET.csv"
+    output_file = "data/Cleaned_Youtube_Channels_Dataset.csv"
     
-    try:
-        process_excel(file_path)
-    except FileNotFoundError as e:
-        print(e)
+    process_data(input_file, output_file)
+    print(f"Data cleaned and saved to {output_file}")
